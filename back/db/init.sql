156bf6a43abb821e4fb5b8b226824d34e1a11e46
Maksymiljan Stankevic <maksymiljan.stankevic@stud.techin.lt>
2026-03-19 13:03:15 +0200

    Fixed Init.sql fail

--- back/db/init.sql
+++ back/db/init.sql
@@ -1,11 +1,11 @@
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE categories (
+CREATE TABLE IF NOT EXISTS categories (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     type ENUM('income', 'expense') NOT NULL,
@@ -14,26 +14,26 @@ CREATE TABLE categories (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE income (
+CREATE TABLE IF NOT EXISTS income (
     id INT PRIMARY KEY AUTO_INCREMENT,
     amount DECIMAL(10,2) NOT NULL,
     description VARCHAR(255),
     date DATE NOT NULL,
     user_id INT NOT NULL,
     category_id INT,
-
+    
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
-CREATE TABLE expenses (
+CREATE TABLE IF NOT EXISTS expenses (
     id INT PRIMARY KEY AUTO_INCREMENT,
     amount DECIMAL(10,2) NOT NULL,
     description VARCHAR(255),
     date DATE NOT NULL,
     user_id INT NOT NULL,
     category_id INT,
-
+    
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (category_id) REFERENCES categories(id)
 );
