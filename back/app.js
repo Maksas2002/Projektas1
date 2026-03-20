780e13cee4ba9c5a8585d508d9e6ce57f07fc95c
Aleksandr Dubovik <aleksandr.dubovek@gmail.com>
2026-03-20 12:29:56 +0200

    app.js

---
+++ back/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+export const PORT = process.env.PORT || 5000;
+export const CLIENT_URL = process.env.CLIENT_URL;
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    credentials: true,
+  })
+);
+
+
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+app.get("/", (req, res) => {
+  res.json({ status: "server ok" });
+});
+
+export default app;
