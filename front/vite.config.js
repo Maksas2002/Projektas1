8232b4ed3b75cf03404655b3d1468da5429b532b
Aleksandr Dubovik <aleksandr.dubovek@gmail.com>
2026-03-23 12:40:34 +0200

    viteConfig

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,5 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: '0.0.0.0',
+    port: 5173,
+  },
   plugins: [react(), tailwindcss(),],
 })
