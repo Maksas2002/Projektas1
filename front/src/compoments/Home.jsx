b4521c066cf6ed27dce919fcb1846f7d32ba8906
Titas-titas <titas.kucinskas@gmail.com>
2026-03-24 15:05:29 +0200

    Added text to Home

--- front/src/compoments/Home.jsx
+++ front/src/compoments/Home.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router";
+
 function Home() {
     return(
         <>
-            <h1 className="text-3xl text-center text-purple-800 py-3">Hi there Docker is ready!!!</h1>
+        <div className="min-h-screen flex flex-col justify-center text-center items-center">
+            <div>
+                <h1 className="text-4xl text-center py-3">Take Control of Your Finances with Ease</h1>
+                <p className="text-center">BudgetNest helps you track expenses, plan savings, and reach your financial goals — all in one beautiful dashboard.</p>
+            </div>
+            <Link to="signup" className="border p-3">Get Started</Link>
+        </div>
         </>
     )
 }
