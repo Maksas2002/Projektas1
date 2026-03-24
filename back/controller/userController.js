2618b7a0037d360c4ede0b51659fa5be8e457175
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-03-24 08:05:25 +0200

    delete commented out code

--- back/controller/userController.js
+++ back/controller/userController.js
@@ -64,10 +64,6 @@ export const signup = async (req, res, next) => {
 export const loginC = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-// console.log(email);
-//     if (email || password) {
-//       throw new AppError("Missing email or password", 404);
-//     }
 
     const user = await getUserByEmailM(email);
     if (!user) throw new AppError("Invalid user email or password", 401);
