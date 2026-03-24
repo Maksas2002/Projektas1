2f7b5bc5bfffa781c7ad3604c1da0f78d1c4c853
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-03-24 08:21:02 +0200

    add validation for login, add validation error handler, add validation folder

---
+++ back/validation/validate.js
@@ -0,0 +1,15 @@
+import { validationResult } from "express-validator";
+
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            status: "fail",
+            error: errors.array(),
+        });
+    }
+    next();
+};
+
+export default validate;
