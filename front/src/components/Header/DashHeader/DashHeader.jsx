8e529f37c05cf9e86562b5f1aea4427ecc270795
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-03-27 23:30:51 +0200

    fix design after merge bacause it didn't merge correctly

--- front/src/components/Header/DashHeader/DashHeader.jsx
+++ front/src/components/Header/DashHeader/DashHeader.jsx
@@ -1,6 +1,7 @@
 import DeleteAccount from "./DeleteAccount";
 import UserLogout from "./UserLogout";
 import { useState } from "react";
+import exit from "../../../assets/box-arrow-right.svg";
 
 function DashHeader() {
     const [show, setShow] = useState(false);
@@ -16,22 +17,22 @@ function DashHeader() {
 
     return (
         <>
-            <header className="flex justify-around pt-5">
+            <header className="bg-[#020b33] pb-2 pt-2 text-white flex justify-around items-center border-b-white/10">
                 <div>
-                    <h2>My Dashboard</h2>
-                    <p>Personal Finance Overview</p>
+                    <h2 className="text-2xl font-semibold text-blue-400 ">My Dashboard</h2>
+                    <p className="text-[1.2rem]">Personal Finance Overview</p>
                 </div>
-                <div>
-                    <button onClick={() => toShow()} type="button">Exit</button>
+                <div className="flex">
+
+                    <button onClick={() => toShow()} className="flex flex-row-reverse items-center gap-2 text-sm font-medium cursor-pointer bg-blue-500 hover:bg-blue-400 px-5 py-2 rounded-[10px]" type="button"><span className="text-[1rem] font-medium">Exit</span>
+                        <img src={exit} alt="exit" className="w-4 h-4 " />
+                    </button>
+
                     <button onClick={() => setShowDelete(true)} className="bg-red-400 text-white hover:bg-red-500 mx-3 py-3 px-5 text-sm rounded-xl font-bold">Delete your Account</button>
                 </div>
-                {show ? <UserLogout notToShow={() => notToShow}/> : null}
-                {showDelete && (<DeleteAccount setShowDelete={setShowDelete}/>)}
+                {show ? <UserLogout notToShow={() => notToShow} /> : null}
+                {showDelete && (<DeleteAccount setShowDelete={setShowDelete} />)}
             </header>
-
-            
-
-            
         </>
     );
 }
