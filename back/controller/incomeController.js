0f4ec4b3b8d924442fc35efd1ce3aa8b54420e66
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-03-30 12:03:50 +0300

    add incomeOntroller.js, creatIncomeC and add incomeModel, createIncomeM

---
+++ back/controller/incomeController.js
@@ -0,0 +1,24 @@
+import appError from "../utils/appError.js";
+import { createIncomeM } from "../modules/incomeModule.js";
+import AppError from "../utils/appError.js";
+
+export const createIncomeC = async (req, res, next) => {
+  try {
+    const newData = req.body;
+    const { id } = req.params;
+
+    if (!newData.amount || !newData.description || !newData.date) {
+      throw new AppError("Error, not enough info", 400);
+    } else if (!newData) {
+      throw new AppError("Error", 400);
+    }
+
+    const post = await createIncomeM(newData, {id});
+    res.status(201).json({
+      status: "success",
+      data: post,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
