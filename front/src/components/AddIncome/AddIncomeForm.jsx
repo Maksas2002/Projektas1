4668c44dcd3b0a0c58df22c5b101586b6c1f9173
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-03-31 11:56:21 +0300

    add the form for posting income, add front validation

--- front/src/components/AddIncome/AddIncomeForm.jsx
+++ front/src/components/AddIncome/AddIncomeForm.jsx
@@ -20,7 +20,33 @@ function AddIncomeForm() {
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" className="border" {...register("amount", {required: true,})}/>
+        <label className="block">Amount (EUR)</label>
+
+        <input
+          type="number"
+          className="border"
+          placeholder="0.00"
+          {...register("amount", {
+            required: true,
+            min: { value: 0, message: "Number cannot be negative" },
+          })}
+        />
+
+        <label className="block">Description</label>
+        <input
+          type="text"
+          className="border"
+          {...register("description", { maxLength: 255 })}
+        />
+
+        <label className="block">Date</label>
+        <input
+          type="date"
+          className="border"
+          {...register("date", { required: true })}
+        />
+
+        <input type="submit" className="block border" value="Add Income" />
       </form>
     </>
   );
