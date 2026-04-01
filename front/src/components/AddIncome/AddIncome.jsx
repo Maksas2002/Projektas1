08943de646ba9fc9dbc8132913f35cdba84420a7
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-04-01 07:53:28 +0300

    add buuton to show or hide income form

--- front/src/components/AddIncome/AddIncome.jsx
+++ front/src/components/AddIncome/AddIncome.jsx
@@ -1,10 +1,19 @@
 import AddIncomeForm from "./AddIncomeForm";
+import { useState } from "react";
 
 function AddIncome() {
+  const [show, setShow] = useState(false);
+
+  const toShow = () => {
+    show ? setShow(false) : setShow(true);
+  };
+
   return (
     <>
       <section className="border p-8 ml-2 w-[45%]">
-        <AddIncomeForm />
+        <button type="button" onClick={toShow}>Add Income</button>
+
+        {show ? <AddIncomeForm /> : null}
       </section>
     </>
   );
