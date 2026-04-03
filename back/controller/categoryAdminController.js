f585e341c873d16957d09ca4283e78137fcd87e5
Titas-titas <titas.kucinskas@gmail.com>
2026-04-03 12:59:54 +0300

    Added funcion for add category

--- back/controller/categoryAdminController.js
+++ back/controller/categoryAdminController.js
@@ -1,9 +1,33 @@
-import { getAllCategoriesM } from "../modules/categoryModule.js";
+import { createCategoryM, getAllCategoriesM, getCategoryByNameM } from "../modules/categoryModule.js";
 
-export const createCategory = async (req, res) => {
-    res.status(200).json({
-      message: "Test",
-    });
+export const createCategory = async (req, res, next) => {
+    try {
+      const {name, type, user_id} = req.body;
+
+      if (!name || !type) {
+        return res.status(400).json({
+          message: "Name and type are required",
+        });
+      }
+
+      //patikrina ar existuoja vardas
+      const existing = await getCategoryByNameM(name);
+      if (existing) {
+        return res.status(409).json({
+          message: "Category name already exists",
+        });
+      }
+
+      //sukurema kategorija
+      const category = await createCategoryM(name, type, user_id || null);
+
+      res.status(200).json({
+        message: "category created successfully",
+        data: category
+      });
+    } catch (error) {
+      next(error)
+    }
 };
 export const getCategories = async (req, res, next) => {
     try {
--- back/modules/categoryModule.js
+++ back/modules/categoryModule.js
@@ -7,5 +7,22 @@ export const getAllCategoriesM = async () => {
   return categoryLists;
 };
 
+//add category
+export const createCategoryM = async (name, type, user_id = null) => {
+  const result = await sql`
+    INSERT INTO categories (name, type, user_id)
+    VALUES (${name}, ${type}, ${user_id})
+    RETURNING *;
+  `;
+  return result[0];
+};
+
+//get all by names
+export const getCategoryByNameM = async (name) => {
+  const result = await sql`
+    SELECT * FROM categories WHERE name = ${name};
+  `;
+  return result[0];
+};
 
 
