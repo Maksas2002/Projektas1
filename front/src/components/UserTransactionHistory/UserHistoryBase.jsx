a9a18f3f9234cd9344493e834fd912a6fe8b9113
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-04-14 11:20:24 +0300

    add fetchTransaction and upload the data to context

--- front/src/components/UserTransactionHistory/UserHistoryBase.jsx
+++ front/src/components/UserTransactionHistory/UserHistoryBase.jsx
@@ -1,23 +1,36 @@
 import UserTransactionTable from "./UserTransactionTable";
+import axios from "axios";
+import errorHandler from "../../utils/errorHandler";
+import { useContext, useEffect, useState } from "react";
+import { TransactionContext } from "../../utlis/TransactionContext";
 
 function UserHistoryBase() {
+  const { setTransaction } = useContext(TransactionContext);
+  const [error, setError] = useState(null);
+
   const fetchTransactions = async () => {
-      try {
-      const response = await axios.get(
-        `${API_URL}/`,
-        {
-          withCredentials: true,
-        },
-      );
-      // console.log(response.data.data);
+    try {
+      const response = await axios.get(`http://localhost:3000/api/v1/user/history`, {
+        withCredentials: true,
+      });
+    //   const income = response.data.incomeData
+    //   const expenses = response.data.incomeData.expensesData
+    //   console.log({income});
+      setTransaction(response.data.incomeData);
     } catch (error) {
+        console.log(error);
       setError(errorHandler(error));
     }
   };
 
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
   return (
     <>
       <section className="flex justify-center border-red-500 border ml-200 mr-200">
+        <p className="text-red text-center">{error}</p>
         <UserTransactionTable />
       </section>
     </>
