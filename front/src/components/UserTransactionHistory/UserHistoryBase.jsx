30076268c1b4ed92fc3e9ba28b0263110ca4015a
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-04-14 11:43:47 +0300

    add first fetch version (need to fix so that both obj arrays would show

--- front/src/components/UserTransactionHistory/UserHistoryBase.jsx
+++ front/src/components/UserTransactionHistory/UserHistoryBase.jsx
@@ -6,19 +6,23 @@ import { TransactionContext } from "../../utlis/TransactionContext";
 
 function UserHistoryBase() {
   const { setTransaction } = useContext(TransactionContext);
+  const transaction = useContext(TransactionContext);
   const [error, setError] = useState(null);
 
   const fetchTransactions = async () => {
     try {
-      const response = await axios.get(`http://localhost:3000/api/v1/user/history`, {
-        withCredentials: true,
-      });
-    //   const income = response.data.incomeData
-    //   const expenses = response.data.incomeData.expensesData
-    //   console.log({income});
-      setTransaction(response.data.incomeData);
+      const response = await axios.get(
+        `http://localhost:3000/api/v1/user/history`,
+        {
+          withCredentials: true,
+        },
+      );
+      //   const income = response.data.incomeData
+      //   const expenses = response.data.incomeData.expensesData
+        console.log(response.data.incomeData);
+    //   setTransaction(response.data.incomeData);
     } catch (error) {
-        console.log(error);
+      console.log(error);
       setError(errorHandler(error));
     }
   };
@@ -29,9 +33,14 @@ function UserHistoryBase() {
 
   return (
     <>
-      <section className="flex justify-center border-red-500 border ml-200 mr-200">
+      <section className="flex flex-col items-center justify-center border-red-500 border ml-200 mr-200">
         <p className="text-red text-center">{error}</p>
-        <UserTransactionTable />
+        {transaction.transaction.map((transaction) => (
+          <UserTransactionTable
+            key={transaction.id}
+            transaction={transaction}
+          />
+        ))}
       </section>
     </>
   );
