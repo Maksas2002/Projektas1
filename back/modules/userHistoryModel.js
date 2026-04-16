5b3ebc89822ce995e592d74da29cef1856e4d37b
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-04-16 10:52:55 +0300

    change query so that it could order by log date

--- back/modules/userHistoryModel.js
+++ back/modules/userHistoryModel.js
@@ -4,28 +4,34 @@ import { sql } from "../dbConnection.js";
 export const userCombinedHistoryM = async (id) => {
   const userCombinedHistory = await sql`
 SELECT 
-    expenses.*, 
+    expenses.*, logs.created_at, logs.id,
     TO_CHAR(expenses.date, 'YYYY-MM-DD') AS formatted_date, 
     categories.name AS category_name,
     'expense' AS type
 FROM expenses
 LEFT JOIN categories
     ON expenses.category_id = categories.id
+LEFT JOIN logs
+ON expenses.id = logs.id
 WHERE expenses.user_id = ${id}
 
 UNION ALL
 
 SELECT 
-    income.*, 
+    income.*, logs.created_at, logs.id,
     TO_CHAR(income.date, 'YYYY-MM-DD') AS formatted_date, 
     categories.name AS category_name,
     'income' AS type
 FROM income
 LEFT JOIN categories
     ON income.category_id = categories.id
+LEFT JOIN logs
+ON income.id = logs.id
 WHERE income.user_id =  ${id}
 
-ORDER BY formatted_date DESC;
+ORDER BY created_at DESC;
+
+
 `;
 
   return userCombinedHistory;
