01f2e466f9dc41eea5013b28ef95d6bd7c55cf50
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-04-16 11:56:49 +0300

    add timestamp DEFAULT CURRENT_TIMESTAMP for income and expenses tables, querie gets transaction from last transaction uploaded

--- back/modules/userHistoryModel.js
+++ back/modules/userHistoryModel.js
@@ -4,29 +4,25 @@ import { sql } from "../dbConnection.js";
 export const userCombinedHistoryM = async (id) => {
   const userCombinedHistory = await sql`
 SELECT 
-    expenses.*, logs.created_at, logs.id,
+    expenses.*,
     TO_CHAR(expenses.date, 'YYYY-MM-DD') AS formatted_date, 
     categories.name AS category_name,
     'expense' AS type
 FROM expenses
 LEFT JOIN categories
     ON expenses.category_id = categories.id
-LEFT JOIN logs
-ON expenses.id = logs.id
 WHERE expenses.user_id = ${id}
 
 UNION ALL
 
 SELECT 
-    income.*, logs.created_at, logs.id,
+    income.*, 
     TO_CHAR(income.date, 'YYYY-MM-DD') AS formatted_date, 
     categories.name AS category_name,
     'income' AS type
 FROM income
 LEFT JOIN categories
     ON income.category_id = categories.id
-LEFT JOIN logs
-ON income.id = logs.id
 WHERE income.user_id =  ${id}
 
 ORDER BY created_at DESC;
