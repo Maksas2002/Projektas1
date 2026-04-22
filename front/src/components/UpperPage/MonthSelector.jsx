eed2ad376cfce67c5a2cb9123f9b3ba3c36781fc
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-04-22 11:53:41 +0300

    fix date selection, now it doesnt crash when all transactions are deleted

--- front/src/components/UpperPage/MonthSelector.jsx
+++ front/src/components/UpperPage/MonthSelector.jsx
@@ -1,13 +1,27 @@
 function MonthSelector({ transaction }) {
-const uniqueMonths = transaction?.transactions?.length
-  ? [...new Set(transaction.transactions.map(t => t.formatted_date.slice(0, 7)))]
-  : [];
+  //  this loop makes the dates appear in the selection. I dont know why it doesnt work without this
+  //  pushes all objects into array
+  const getAllFormattedDates = () => {
+    let list = [];
+    for (let i = 0; i < transaction.length; i++) {
+      list.push(transaction[i]);
+    }
+    return list;
+  };
+
+  const uniqueMonths = [
+    ...new Set(
+      getAllFormattedDates()
+        .map((t) => t.formatted_date?.slice(0, 7))
+        .filter(Boolean),
+    ),
+  ];
 
   return (
     <>
       {uniqueMonths.map((month) => (
         <option key={month} value={month} className="text-white">
-          {month ? month : null}
+          {month}
         </option>
       ))}
     </>
