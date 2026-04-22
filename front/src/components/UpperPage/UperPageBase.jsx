825b0f006a5ff7a866fe5144c5983ea554016b53
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-04-22 07:56:49 +0300

    add getUserName function to het name from context when obj changes

--- front/src/components/UpperPage/UperPageBase.jsx
+++ front/src/components/UpperPage/UperPageBase.jsx
@@ -1,22 +1,29 @@
 import { useContext } from "react";
 import { UserContext } from "../../utlis/UserContext";
 
-
 function UpperPageBase() {
-    const user = useContext(UserContext);
+  const user = useContext(UserContext);
 
-    return (
-        <>
-            <section className="flex justify-around">
-                <div>
-                    <h1 className="text-white text-[1.5rem]">Welckome back, {user.user.data.name}</h1>
-                    <p className="text-sky-400">Here's your financial overview for month</p>
-                </div>
+  // get name from context
 
-                
-            </section>
-        </>
-    );
+  const getUserName = () => {
+    return user?.user?.name || user?.user?.data?.name || null;
+  };
+
+  return (
+    <>
+      <section className="flex justify-around">
+        <div>
+          <h1 className="text-white text-[1.5rem]">
+            Welckome back, {getUserName()}
+          </h1>
+          <p className="text-sky-400">
+            Here's your financial overview for month
+          </p>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default UpperPageBase;
