22cb97b5688540e81adad72e74af60f81396d5c1
ArBa-dev-J <balevicius.arminas@gmail.com>
2026-04-23 10:54:13 +0300

    add balance table to show users monthly balance

--- front/src/components/MonthlyFInancies/MonthlyBalance.jsx
+++ front/src/components/MonthlyFInancies/MonthlyBalance.jsx
@@ -1,13 +1,43 @@
-function MonthlyBalance(){
-return(
+import axios from "axios";
+import errorHandler from "../../utils/errorHandler";
+import { BalanceContext } from "../../utlis//BalanceContext";
+import { TransactionContext } from "../../utlis/TransactionContext";
+import { useContext, useEffect, useState } from "react";
+import { MonthContext } from "../../utlis/MonthContext";
+
+function MonthlyBalance() {
+  const month = useContext(MonthContext);
+  const { transaction } = useContext(TransactionContext);
+  const { balance, setBalance } = useContext(BalanceContext);
+  const [error, setError] = useState(null);
+
+  const getBalance = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/v1/user/${month.month}-1/totalBalance`,
+        {
+          withCredentials: true,
+        },
+      );
+      setBalance(response.data.userMonthlyBalance);
+    } catch (error) {
+      setError(errorHandler(error));
+    }
+  };
+
+  useEffect(() => {
+    getBalance();
+  }, [month.month, transaction]);
+  return (
     <>
-    <section className="border border-[#061a75] bg-linear-to-br from-[#020b33] to-[#14215a] rounded-[20px] p-8 mt-5 w-45">
-        <p>Balance</p>
-        <p>Money</p>
-        <p>Smaller Money text</p>
-    </section>
+      <section className="border border-[#061a75] bg-linear-to-br from-[#020b33] to-[#14215a] rounded-[20px] p-8 mt-5 w-45">
+        <p className="text-sky-400 pb-2 text-[0.8rem]">Balance</p>
+        <p className="text-white text-[1.5rem] pb-2">€{balance}</p>
+        <p className="text-sky-400 text-[0.8rem]">{balance}</p>
+        <p className="text-red-500">{error}</p>
+      </section>
     </>
-);
+  );
 }
 
 export default MonthlyBalance;
