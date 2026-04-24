7d264fd90efd633a260e973fb7892a17af1d215d
Titas-titas <titas.kucinskas@gmail.com>
2026-04-24 16:43:33 +0300

    added date in expense total

--- front/src/components/ExpensesTotal/ExpensesTotal.jsx
+++ front/src/components/ExpensesTotal/ExpensesTotal.jsx
@@ -7,14 +7,23 @@ function ExpensesTotal() {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [month, setMonth] = useState("2026-04");
 
-    const startDate = '2026-04-01';
-    const endDate = '2026-04-30';
+    const getDateRange = (monthString) => {
+        const [year, month] = monthString.split("-");
+        const startDate = `${year}-${month}-01`;
+        const lastDay = new Date(year, month, 0).getDate();
+        const endDate = `${year}-${month}-${String(lastDay).padStart(2, "0")}`;
+        return { startDate, endDate };
+    };
 
     const fetchCategoryTotal = async () => {
       try {
         const user = JSON.parse(localStorage.getItem("user"));
         const userId = user?.data?.id;
+
+        const { startDate, endDate } = getDateRange(month);
+
         console.log(userId)
         const res = await axios.get(`http://localhost:3000/api/v1/user/${userId}/expenses/byCategory?startDate=${startDate}&endDate=${endDate}`,
             {
@@ -34,13 +43,22 @@ function ExpensesTotal() {
 
     useEffect(() => {
         fetchCategoryTotal()
-    }, [])
+    }, [month])
     return(
         <section className="flex flex-col items-center justify-center pt-3 pb-3 gap-2 rounded-[13px] border-[#061a75] bg-[#020b33] border w-full max-w-185 mx-auto">
             {error && <p className="text-red-500 text-center">{error}</p>}
-            <p className="text-white self-baseline pl-26 text-[1.2rem]">
-                Expenses Total 
-            </p>
+            <div className="flex w-full justify-around items-center">
+                <p className="text-white self-baseline text-[1.2rem]">
+                    Expenses Total 
+                </p>
+                <input
+                    type="month"
+                    value={month}
+                    onChange={(e) => setMonth(e.target.value)}
+                    className="bg-[#1f2747] text-white p-2 rounded"
+                />
+            </div>
+
             <ExpensesByCategory data={data} loading={loading}/>
             
         </section>
